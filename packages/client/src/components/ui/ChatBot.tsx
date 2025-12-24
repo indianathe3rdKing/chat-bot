3353589d13515c8dde56ef23f9a0113a2a47fc1f
indianathe3rd <indiemenziwa@gmail.com>
2025-12-24 19:18:24 +0200

    Connecting to Backend

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,20 +1,27 @@
+import axios from 'axios';
 import { Button } from './button';
 import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
-import type { KeyboardEvent } from 'react';
+import { useRef, type KeyboardEvent } from 'react';
 
 type FormData = {
    prompt: string;
 };
 
 const ChatBot = () => {
+   const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, watch, formState } =
       useForm<FormData>();
    watch('prompt');
 
-   const onSubmit = (data: FormData) => {
-      console.log(data);
+   const onSubmit = async ({ prompt }: FormData) => {
       reset();
+      const { data } = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+
+      console.log(data);
    };
 
    const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
