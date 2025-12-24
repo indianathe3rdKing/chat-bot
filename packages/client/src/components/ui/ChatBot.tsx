905780a79e54cf99e28298a3149cf7eb532cc9ff
indianathe3rd <indiemenziwa@gmail.com>
2025-12-24 18:52:39 +0200

    Handling Form Submission

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,16 +1,47 @@
 import { Button } from './button';
 import { FaArrowUp } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import type { KeyboardEvent } from 'react';
+
+type FormData = {
+   prompt: string;
+};
+
 const ChatBot = () => {
+   const { register, handleSubmit, reset, watch, formState } =
+      useForm<FormData>();
+   watch('prompt');
+
+   const onSubmit = (data: FormData) => {
+      console.log(data);
+      reset();
+   };
+
+   const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         handleSubmit(onSubmit)();
+      }
+   };
+
    return (
-      <div className="flex flex-col gap-2 items-end border-2 p-5 rounded-2xl">
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
+         className="flex flex-col gap-2 items-end border-2 p-5 rounded-2xl"
+      >
          <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
          />
-         <Button className="rounded-full w-8 h-8">
+         <Button disabled={!formState.isValid} className="rounded-full w-8 h-8">
             <FaArrowUp />
          </Button>
-      </div>
+      </form>
    );
 };
 
