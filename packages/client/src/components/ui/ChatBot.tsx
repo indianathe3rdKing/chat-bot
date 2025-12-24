f362d57d20f643fa13bfd93ee273784fcd434c9b
indianathe3rd <indiemenziwa@gmail.com>
2025-12-24 23:36:19 +0200

    Adjust Token Limit

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { useRef, useState, type KeyboardEvent } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useForm } from 'react-hook-form';
 import { Button } from './button';
 import { FaArrowUp } from 'react-icons/fa';
-import { useForm } from 'react-hook-form';
-import { useRef, useState, type KeyboardEvent } from 'react';
 
 type FormData = {
    prompt: string;
@@ -54,7 +55,7 @@ const ChatBot = () => {
                         : 'bg-gray-100 text-black self-start'
                   }`}
                >
-                  {message.content}
+                  <ReactMarkdown>{message.content}</ReactMarkdown>
                </p>
             ))}
          </div>
