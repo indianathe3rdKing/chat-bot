e8031d33cc44f9e78b3d18def9dfd5ede9f5619f
indianathe3rd <indiemenziwa@gmail.com>
2025-12-24 12:42:35 +0200

    Extracting Conversation Respository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import z from 'zod';
+import { conversationRepository } from './repositories/conversation.repository';
 
 dotenv.config();
 
@@ -22,7 +23,6 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.send({ message: 'Hello from Bun!' });
 });
 
-const conversations = new Map<string, string>();
 const chatSchema = z.object({
    prompt: z
       .string()
@@ -43,14 +43,16 @@ app.post('/api/chat', async (req: Request, res: Response) => {
       const { prompt, conversationId } = req.body;
 
       const response = await client.responses.create({
-         model: 'gpt-4o-mini',
+         model: 'gpt-4o-mini.',
          input: prompt,
          temperature: 0.2,
          max_output_tokens: 100,
-         previous_response_id: conversations.get(conversationId),
+         previous_response_id:
+            conversationRepository.getLastResponseId(conversationId),
       });
 
-      conversations.set(conversationId, response.id);
+      conversationRepository.setLastResponseId(conversationId, response.id);
+
       res.json({ message: response.output_text });
    } catch (error) {
       res.status(500).json({ error: 'Failed to fetch response ' });
---
+++ packages/server/repositories/conversation.repository.ts
@@ -0,0 +1,11 @@
+//Emplementation details for conversation repository
+const conversations = new Map<string, string>();
+
+export const conversationRepository = {
+   getLastResponseId(conversationId: string) {
+      return conversations.get(conversationId);
+   },
+   setLastResponseId(conversationId: string, responseId: string) {
+      return conversations.set(conversationId, responseId);
+   },
+};
