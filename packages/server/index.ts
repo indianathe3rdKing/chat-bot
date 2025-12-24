f0f04aaae6e37893ca8bd83a53c421fcfbfd8674
indianathe3rd <indiemenziwa@gmail.com>
2025-12-24 14:26:50 +0200

    Extracting Routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,23 +1,14 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send(process.env.OPENAI_API_KEY);
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.send({ message: 'Hello from Bun!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Hello Bun on port http://localhost:${port}`);
