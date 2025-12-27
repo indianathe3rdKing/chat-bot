8fce285243dc59ba9cc298a51d738abf89f065d4
indianathe3rd <indiemenziwa@gmail.com>
2025-12-27 13:38:01 +0200

    Updating Vite Config Proxy

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
    },
    server: {
       proxy: {
-         '/api': 'http://localhost:3000',
+         '/api': 'https://chat-bot-1-rv89.onrender.com',
       },
    },
 });
